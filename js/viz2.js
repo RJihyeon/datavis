90d098d6d1c96c739ad441f3046261f905c8dc69
Daun Jung <dw0620@yonsei.ac.kr>
2024-05-09 04:28:20 +0900

    modified

--- js/viz2.js
+++ js/viz2.js
@@ -1,19 +1,93 @@
-(function(){// 스크립트의 변수를 로컬로 제한하는 IIFE방식으로 충돌 방지
+const width = 900;
+const height = 500;
+const margin = 30;
+const legendHeight = 50;
 
-//아래부터 쓰시면 됩니다.  아래 코드는 예시입니다.
-const svg = d3.select("#viz2").append("svg")
-    .attr("width", 600)
-    .attr("height", 400);
+d3.csv("./data_famtype/element_alone.csv").then((data) => {
+    showStackedBarChart(data);
+});
 
-svg.selectAll("rect")
-    .data([1770, 1544, 2330, 1125, 310])
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => i * 40)
-    .attr("y", d => 400 - 10 * d)
-    .attr("width", 35)
-    .attr("height", d => 10 * d)
-    .attr("fill", "teal");
+function getDomain(data) {
+    const categories = data.map(d => d.category);
+    return categories;
+}
 
-    console.log("SVG added", svg);
-})();
+function getRange(data) {
+    const uniquecolor = [...new Set(data.map(d => d.color))];
+    return uniquecolor;
+}
+
+function showStackedBarChart(data) {
+    // VARIABLES
+    const firstRow = data[0];
+    const xLabel = Object.keys(firstRow)[0];
+    const stackKeys = Object.keys(firstRow).filter(key => key !== xLabel);
+    const columnCount = Object.keys(firstRow).length;
+    const category = [];
+    for (let i = 1; i < columnCount; i++) {
+        category[i - 1] = Object.keys(firstRow)[i];
+    }
+
+    // COLOR
+    const domain = getDomain(data);
+    const range = getRange(data);
+
+    const color = d3.scaleOrdinal(d3.schemeCategory10)
+        .domain(data.map(d => d.categories));
+
+    // SVG
+    const svg = d3
+        .select("body")
+        .append("svg")
+        .attr("width", width + margin + margin)
+        .attr("height", height + margin + margin + legendHeight)
+        .append("g")
+        .attr("transform", `translate(${margin}, ${margin})`);
+
+    const xScale = d3
+        .scaleBand()
+        .domain(data.map((d) => d[xLabel]))
+        .range([0, width])
+        .paddingInner(0.4)
+        .paddingOuter(0.4);
+
+    const yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, (d) => d3.sum(Object.values(d).slice(1)))])
+        .range([height, 0]);
+
+    const xAxis = d3.axisBottom().scale(xScale);
+    const xAxisGroup = svg
+        .append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .attr("class", "xAxis-style")
+        .call(xAxis);
+
+    const yAxis = d3.axisLeft().scale(yScale);
+    const yAxisGroup = svg.append("g").call(yAxis);
+
+    const stack = d3.stack()
+        .keys(stackKeys)
+        .order(d3.stackOrderNone)
+        .offset(d3.stackOffsetNone);
+
+    let stacked = stack(data);
+
+    let bars = svg.selectAll(".bar")
+        .data(stacked)
+        .enter()
+        .append("g")
+        .attr("class", "bar")
+        .attr("fill", (d, i) => color(d.key))
+        .selectAll("rect")
+        .data(d => d)
+        .enter();
+
+    const rect = bars.append("rect")
+        .attr("x", d => xScale(d.data[xLabel]))
+        .attr("y", d => yScale(d[1]))
+        .attr("height", d => yScale(d[0]) - yScale(d[1]))
+        .attr("width", (xScale.bandwidth()))
+        .attr("data-xLabel", d => d.data[xLabel])
+        .attr("data-product", d => d.key);
+}
