2df808d584b197816a65c29d1ea64fd35bb1e690
Daun Jung <dw0620@yonsei.ac.kr>
2024-05-09 05:29:26 +0900

    update

--- js/viz2.js
+++ js/viz2.js
@@ -1,25 +1,19 @@
-const width = 900;
-const height = 500;
-const margin = 30;
-const legendHeight = 50;
+(function(){// 스크립트의 변수를 로컬로 제한하는 IIFE방식으로 충돌 방지
 
-// d3.csv("./data_famtype/test_simple.csv").then((dataset) => {
-//     showBarChart(dataset);
-// });
-
-//     //아래부터 쓰시면 됩니다. 아래 코드는 예시입니다.
-const svg = d3.select("#viz2").append("svg")
-    .attr("width", width + margin + margin)
-    .attr("height", height + margin + margin + legendHeight)
-    .append("g")
-    .attr("transform", `translate(${margin}, ${margin})`);
-
-svg.selectAll("rect")
-    .data([3, 9, 4, 2])
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => i * 40)
-    .attr("y", d => 400 - 10 * d)
-    .attr("width", 35)
-    .attr("height", d => 10 * d)
-    .attr("fill", "teal");
+    //아래부터 쓰시면 됩니다. 아래 코드는 예시입니다.
+  const svg = d3.select("#viz2").append("svg")
+      .attr("width", 600)
+      .attr("height", 400);
+  
+  svg.selectAll("rect")
+      .data([10, 15, 20, 233, 30])
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 40)
+      .attr("y", d => 400 - 10 * d)
+      .attr("width", 35)
+      .attr("height", d => 10 * d)
+      .attr("fill", "teal");
+  
+      console.log("SVG added", svg);
+  })();
