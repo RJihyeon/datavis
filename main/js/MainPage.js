aea15334d063111c3364619cea56f9e0985f832d
Daun Jung <dw0620@yonsei.ac.kr>
2024-05-28 12:17:39 +0900

    main update

--- main/js/MainPage.js
+++ main/js/MainPage.js
@@ -4,7 +4,29 @@ document.addEventListener("DOMContentLoaded", function() {
     contentArea.innerHTML = ""; // Clear previous content
     switch (activeComponent) {
       case "family-type":
-        contentArea.innerHTML = "<div>가정유형 콘텐츠</div>"; // Replace with actual HTML content
+        contentArea.innerHTML = `
+          <div>
+            <button data-src="./data/kids_alone(o).csv">미취학 자녀가 혼자 있는 시간</button>
+            <button data-src="./data/element_alone(o).csv">초등학생 자녀가 혼자 있는 시간</button>
+            <button data-src="./data/middle_alone(o).csv">중학생 이상 자녀가 혼자 있는 시간</button>
+          </div>
+          <div id="data-container"></div>
+          <form id="dataSelect">
+            <input type="button" data-group="g1" value="한부모 연령별">
+            <input type="button" data-group="g2" value="한부모 학력별">
+            <input type="button" data-group="g3" value="혼인 상태별">
+            <input type="button" data-group="g4" value="가구 구성별">
+            <input type="button" data-group="g5" value="가장 어린 자녀별">
+            <input type="button" data-group="g6" value="종사상 지위별">
+            <input type="button" data-group="g7" value="정부 지원 유형별">
+            <input type="button" data-group="g8" value="소득 수준별">
+            <input type="button" data-group="g9" value="한부모가된 기간별">
+          </form>
+        `;
+        // Append script dynamically
+        const script = document.createElement("script");
+        script.src = "js/stacked.js";
+        contentArea.appendChild(script);
         break;
       case "dom-violence":
         contentArea.innerHTML = "<div>가정환경 콘텐츠</div>"; // Replace with actual HTML content
