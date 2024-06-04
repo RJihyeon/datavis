15188708efaf13d2006811eb5aee190c41553182
Daun-Jung <102778801+Daun-Jung@users.noreply.github.com>
2024-06-04 20:31:23 +0900

    daun update

--- main/js/MenuComponent.js
+++ main/js/MenuComponent.js
@@ -8,40 +8,40 @@ function renderMenuComponent() {
           <button class="menu fst_button">청소년 가출 현황</button>
           <ul class="sub_menu">
             <li>
-              <button class="sub_button click" id="runaway" onclick ="setActiveComponent('runaway')">- 가출 여부</button>
+              <div class="sub_button click" id="runaway" onclick="setActiveComponent('runaway')">- 가출 여부</div>
             </li>
           </ul>
         </li>
       </ul>
       <ul>
-          <li class="side-nav__list on">
-            <button class="menu fst_button">잠재적 원인 관련 데이터</button>
-            <ul class="sub_menu">
-              <li>
-                <button class="sub_button click" id="famtype" onclick="setActiveComponent('family-type')">- 가정유형</button>
-              </li>
-              <li>
-                <button class="sub_button click" id="domviolence" onclick="setActiveComponent('dom-violence')">- 가정폭력</button>
-              </li>
-              <li>
-                <button class="sub_button click" id="school" onclick="setActiveComponent('school-violence')">- 학교폭력</button>
-              </li>
-            </ul>
-          </li>
+        <li class="side-nav__list on">
+          <button class="menu fst_button">잠재적 원인 관련 데이터</button>
+          <ul class="sub_menu">
+            <li>
+              <div class="sub_button click" id="famtype" onclick="setActiveComponent('family-type')">- 한부모 가정</div>
+            </li>
+            <li>
+              <div class="sub_button click" id="domviolence" onclick="setActiveComponent('dom-violence')">- 가정폭력</div>
+            </li>
+            <li>
+              <div class="sub_button click" id="school" onclick="setActiveComponent('school-violence')">- 학교폭력</div>
+            </li>
+          </ul>
+        </li>
       </ul>
       <ul>
-          <li class="side-nav__list on">
-            <button class="menu fst_button">인프라 데이터</button>
-            <ul class="sub_menu">
-              <li>
-                <button class="sub_button click" id="social-infra" onclick="setActiveComponent('social-infra')">- 사회복지 인프라</button>
-              </li>
-              <li>
-                <button class="sub_button click" id="social-infra" onclick="setActiveComponent('violence-infra')">- 가정폭력 교육</button>
-              </li>
-              <li>
-                <button class="sub_button click" id="school" onclick="setActiveComponent('after-school-bully')">- 학교폭력 피해지원</button>
-              </li>              
+        <li class="side-nav__list on">
+          <button class="menu fst_button">인프라 데이터</button>
+          <ul class="sub_menu">
+            <li>
+              <div class="sub_button click" id="social-infra" onclick="setActiveComponent('social-infra')">- 사회복지 인프라</div>
+            </li>
+            <li>
+              <div class="sub_button click" id="social-infra" onclick="setActiveComponent('violence-infra')">- 가정폭력 교육</div>
+            </li>
+            <li>
+              <div class="sub_button click" id="school" onclick="setActiveComponent('after-school-bully')">- 학교폭력 피해지원</div>
+            </li>              
     </div>
   `;
 }
